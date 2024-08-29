cf25ad90c4e51622bde67bee050599f0676d7629
Cristian Gutierrez <instanciageek@gmail.com>
2024-08-29 02:20:14 -0400

    chore: add instrumentation middleware and apply it to endpoints
    - Create instrumentation middleware to log request and response details.
    - Apply the instrumentation middleware to the Express app configuration.
    - Example endpoint demonstrating the use of the middleware.

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import * as itemController from '../controllers/item.controller';
 import {validateRequest} from "../middleware/validate";
 import {getItemSchema, getItemsSchema} from "../validations/item.validation";
+import instrumentationMiddleware from "../middleware/instrumentation";
 
 const router = Router();
 
-router.get('/items', validateRequest(getItemsSchema) ,itemController.getItems);
-router.get('/items/:id', validateRequest(getItemSchema) ,itemController.getItemById);
+router.get('/items', instrumentationMiddleware, validateRequest(getItemsSchema) ,itemController.getItems);
+router.get('/items/:id', instrumentationMiddleware, validateRequest(getItemSchema) ,itemController.getItemById);
 
 export default router
